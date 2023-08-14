71e06beb916fb17c94e4e2f07b505712a30db290
Leebleyz <lynospasi@gmail.com>
2023-08-14 15:16:45 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Return: 1 (Success)
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, 59);
 	return (1);
 }
