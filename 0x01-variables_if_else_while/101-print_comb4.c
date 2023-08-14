942c8d85029ab5169f2434835971b0c478681fdc
Leebleyz <lynospasi@gmail.com>
2023-08-14 04:51:02 -0700

    variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,21 +10,23 @@ int main(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i <= 7; i++)
 	{
 		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				if (i != 7)
+				if (i != 7 && j != 8 && k != 9)
 				{
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
