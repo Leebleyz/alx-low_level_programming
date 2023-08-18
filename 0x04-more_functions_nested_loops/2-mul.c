86e0a31e238d72fb0aafbbbe36baa6327159d551
Leebleyz <lynospasi@gmail.com>
2023-08-18 13:59:06 -0700

    Write a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @a:integer to be multiplied
+ * @b:second interger to be multiplied
+ * Return: a * c if successful
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
