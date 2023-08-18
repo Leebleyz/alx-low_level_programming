6ffad61dd4db8355c9c995f26ed3ef719997273f
Leebleyz <lynospasi@gmail.com>
2023-08-18 13:34:17 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c:integer to display output
+ * Return: 1 if uppercase, 0 if lower
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
