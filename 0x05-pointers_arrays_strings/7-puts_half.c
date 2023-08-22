98c4391ebd82ef974b7b3e0560d63975c3c0eec5
Leebleyz <lynospasi@gmail.com>
2023-08-21 20:46:48 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - function entry point
+ * @str: string to be printed half
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int half_length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	half_length = (length - 1) / 2;
+
+	for (i = half_length; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
